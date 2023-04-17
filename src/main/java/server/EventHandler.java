afcdd54bde3d796e93f7a3ad9e77685d53a85baa
Bernf0 <96490883+Bernf0@users.noreply.github.com>
2023-04-17 16:13:37 -0400

    Javadoc ServerLauncher et EventHandler

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -1,5 +1,8 @@
 package server;
 
+/**
+ * Il s'agit d'une interface fonctionnelle qui permet de définir une méthode handle() pour définir tous les évenements possibles
+ */
 @FunctionalInterface
 public interface EventHandler {
     void handle(String cmd, String arg);
--- src/main/java/server/ServerLauncher.java
+++ src/main/java/server/ServerLauncher.java
@@ -1,8 +1,20 @@
 package server;
 
+/**
+ * La classe permet de faire rouler le serveur
+ */
 public class ServerLauncher {
+    /**
+     * Le port sur lequel le serveur se connecte
+     */
     public final static int PORT = 1337;
 
+    /**
+     * La méthode qui permet de créer et de lancer le serveur pour qu'il interagisse avec le client
+     *
+     * @param args l'argument du terminal
+     * @throws Exception S'il y a une erreur qui se produit lors du traitements des demandes du client, il y a impression de l'exception dans la console.
+     */
     public static void main(String[] args) {
         Server server;
         try {
